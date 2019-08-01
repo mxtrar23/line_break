0b79f3e7e2892d1dc2f50e489855064ec40c3f60
mxtrar23 <43935347+mxtrar23@users.noreply.github.com>
2019-07-31 22:46:08 -0500

    Update index.php
    en proceso

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
 <?php
 function line_break($text,$characters){
-  $output="hola";
+  $output=array(); //salida
+  $words = explode( ' ', $text); //separamos por espacio
+  $TEMP_COUNTchars=0;//caracteres actuales por linea
+  $TEMPchars='';
+  foreach ($words as $key => $value) {//recorremos las palabras
+    $count =strlen($value);
+    if (($count+$TEMP_COUNTchars)<=$characters) {
+      $TEMPchars.=$value.' ';
+      $TEMP_COUNTchars+=($count+1);
+    }else {
+      array_push($output,$TEMPchars);
+      $TEMPchars=$value;
+      $TEMP_COUNTchars+$count;
+    }
+  }
+  if (!empty($TEMPchars)) {
+    array_push($output,$TEMPchars);
+  }
   return $output;
 }
-echo ">>".line_break("hello world",22);
+var_dump(line_break("1234 123 12345 1234 123 12",10));
 ?>
