76d6dc7806d9b47d4f21e06f2a5784c34c361493
ArbeyP <ar23prz.mxtr@gmail.com>
2020-07-20 14:58:40 -0500

    Firma de la funcion

--- index.php
+++ index.php
@@ -1,31 +1,32 @@
 <?php
-function line_break($text,$characters){
-  $output=array(); //salida
-  $words = explode( ' ', $text); //separamos por espacio
-  $TEMP_COUNTchars=0;//caracteres actuales por linea
-  $TEMPchars='';
-  foreach ($words as $key => $value) {//recorremos las palabras
-    $count =strlen($value);//cantidad de caracteres de la palabra
-    if (($count+$TEMP_COUNTchars)<=$characters) {
-      $TEMPchars.=$value.' ';
-      $TEMP_COUNTchars+=($count+1);
-    }else {
-      array_push($output,$TEMPchars);
-      $TEMPchars=$value.' ';
-      $TEMP_COUNTchars=$count+1;
+  //Creado por Arbey Perez
+  function line_break($text,$characters){
+    $output=array(); //salida
+    $words = explode( ' ', $text); //separamos por espacio
+    $TEMP_COUNTchars=0;//caracteres actuales por linea
+    $TEMPchars='';
+    foreach ($words as $key => $value) {//recorremos las palabras
+      $count =strlen($value);//cantidad de caracteres de la palabra
+      if (($count+$TEMP_COUNTchars)<=$characters) {
+        $TEMPchars.=$value.' ';
+        $TEMP_COUNTchars+=($count+1);
+      }else {
+        array_push($output,$TEMPchars);
+        $TEMPchars=$value.' ';
+        $TEMP_COUNTchars=$count+1;
+      }
     }
+    if (!empty($TEMPchars)) {
+      array_push($output,$TEMPchars);
+    }
+    return $output;
   }
-  if (!empty($TEMPchars)) {
-    array_push($output,$TEMPchars);
-  }
-  return $output;
-}
 
-//Example
-$Text="Hola buen día, este es un párrafo de texto que será dividido por este código";
-$Characters=20;
-echo '<b>'.$Text.'</b><br>';
-foreach (line_break($Text,$Characters) as $key => $value) {
-  echo '-- '.$value.'<br>';
-}
+  //Example
+  $Text="Hola buen día, este es un párrafo de texto que será dividido por este código";
+  $Characters=20;
+  echo '<b>'.$Text.'</b><br>';
+  foreach (line_break($Text,$Characters) as $key => $value) {
+    echo '-- '.$value.'<br>';
+  }
 ?>
